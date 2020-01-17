a290f923f1f813084eacdfd478b2764dabc3c958
Denis <Denis-syl@yandex.ru>
2020-01-17 19:40:45 +0500

    Смена картинки и изменение цвета шрифта на заставке

--- TDGame.py
+++ TDGame.py
@@ -67,7 +67,7 @@ class Game:
         font = pg.font.Font(None, 30)
         text_coord = 50
         for line in intro_text:
-            string_rendered = font.render(line, 1, pg.Color('blue'))
+            string_rendered = font.render(line, 1, pg.Color('BLACK'))
             intro_rect = string_rendered.get_rect()
             text_coord += 10
             intro_rect.top = text_coord
